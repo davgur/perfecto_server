08acd8a409fbe0263e3ee3c5ff13129abd8c2da8
davgur <gur28davaravut>
2020-01-30 18:26:25 +0200

    1

---
+++ src/test/java/com/perfecto/davgur/DavgurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfecto.davgur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavgurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
